fb6fae07f200fde2c66230c582f6bd52786fd895
Mundrack <mateopuga75@gmail.com>
2024-10-07 11:06:31 -0500

    cramos la base de datos con SQL server y ademas creamos la DB con Entity Framework

---
+++ Data/MateoPuga_Taller1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MateoPuga_Taller1.Models;
+
+namespace MateoPuga_Taller1.Data
+{
+    public class MateoPuga_Taller1Context : DbContext
+    {
+        public MateoPuga_Taller1Context (DbContextOptions<MateoPuga_Taller1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MateoPuga_Taller1.Models.Burger> Burger { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MateoPuga_Taller1.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MateoPuga_Taller1Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MateoPuga_Taller1Context") ?? throw new InvalidOperationException("Connection string 'MateoPuga_Taller1Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
