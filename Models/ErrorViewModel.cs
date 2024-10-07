509e97a91c478c6882322d676f317dc10c3f5784
Mundrack <mateopuga75@gmail.com>
2024-10-07 10:42:37 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MateoPuga_Taller1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
